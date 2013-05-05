3b07f2d3b020e6a93754ed63f3be3cab93e237b0
Sondre Eikanger Kvalø <zapodot@gmail.com>
2013-05-05 22:43:16 +0200

    Add license and sonatype publish data to build

--- project/Build.scala
+++ project/Build.scala
@@ -15,10 +15,38 @@ object ApplicationBuild extends Build {
     "com.codahale.metrics" % "metrics-annotation" % metricsVersion,
     "junit" % "junit" % "4.11" % "test"
   )
+  val nexus = "https://oss.sonatype.org/"
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     organization := "org.zapodot",
-    publishTo := Some(Resolver.file("file",  new File( "../zapodot.github.io/repo/snapshots" )) )
+    publishMavenStyle := true,
+    publishArtifact in Test := false,
+    publishTo <<= version { (v: String) =>
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    pomExtra := (
+      <url>https://github.com/zapodot/play-metrics</url>
+        <licenses>
+          <license>
+            <name>Apache License</name>
+            <url>http://opensource.org/licenses/Apache-2.0</url>
+            <distribution>repo</distribution>
+          </license>
+        </licenses>
+        <scm>
+          <url>git@github.com:zapodot/play-metrics.git</url>
+          <connection>scm:git:git@github.com:zapodot/play-metrics.git</connection>
+        </scm>
+        <developers>
+          <developer>
+            <id>zapodot</id>
+            <name>Sondre Eikanger Kvalø</name>
+            <url>http://zapodot.github.io</url>
+          </developer>
+        </developers>)
   )
 
 }
